f49956ca2e28600bf9cd6d768f55802371a71439
Frankeleyn <2582726641@qq.com>
2021-11-04 17:18:24 +0800

    ssh key

---
+++ src/com/effective/TestGit.java
@@ -0,0 +1,12 @@
+package com.effective;
+
+/**
+ * @packge: com.effective
+ * @author: Frankeleyn
+ * @date: 2021/11/4 17:12
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("git提交");
+    }
+}
